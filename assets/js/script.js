de281a1e7c56fdbbfcec46085d76de251163de30
VincenzoLivigni <enzolivigni13@gmail.com>
2025-10-27 14:21:24 +0100

    select the DOM elements and retrieve the data with an AJAX call to the API.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
 console.log("hello");
+/*
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API, 
+sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+*/
+
+// creo una variabile dove riporto l'url dell'API
+const url = "https://lanciweb.github.io/demo/api/pictures/"
+// seleziono gli elementi della DOM
+const genera = document.getElementById("genera")
+
+// richiamo i dati con una chiamata AJAX all'API
+axios.get(url)
+.then((res) => {
+console.log(res);
+const datiCards = res.data;
+console.log(datiCards);
+})
+
+// ciclo per poter prendere gli oggetti singoli
+
+// richiamo il contenuto della card
+
+// cambio le proprietà delle card
+
